5aebac73bd891e36f6dc2c2eb393e4db34af41a9
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2026-01-24 03:02:26 +0800

    add a padding to footer when in mobile view

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="py-8 bg-accent border-t text-center">
+    <footer className="py-8 px-5 md:px-0 bg-accent border-t text-center">
       <p className="text-muted-foreground">
         © {new Date().getFullYear()} Eli. All rights reserved. Built with React, TypeScript and
         {" "}
-        <a href="https://www.reactbits.dev/" target='blank' className='text-blue-500 underline'>ReactBits.</a>
+        <a href="https://www.reactbits.dev/" target='blank' className='text-blue-500 underline'>ReactBits</a>.
       </p>
     </footer>
   );
