4282f01fedeabadf5566d0d64e5f6d296cc9a36c
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2026-01-24 02:06:17 +0800

    feat: Update Footer component to include technology attribution and link to ReactBits

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,9 @@ export const Footer: React.FC = () => {
   return (
     <footer className="py-8 bg-accent border-t text-center">
       <p className="text-muted-foreground">
-        © {new Date().getFullYear()} Eli. All rights reserved.
+        © {new Date().getFullYear()} Eli. All rights reserved. Built with React, TypeScript and
+        {" "}
+        <a href="https://www.reactbits.dev/" target='blank' className='text-blue-500 underline'>ReactBits.</a>
       </p>
     </footer>
   );
